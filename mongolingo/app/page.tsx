c08d45af5c8abcd7caa70652f141c2dd7d9475c9
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-14 01:52:24 +0000

    update 1

--- mongolingo/app/page.tsx
+++ mongolingo/app/page.tsx
@@ -1,6 +1,8 @@
-
+import {Button} from "@/components/ui/button"
 export default function Home() {
   return(
-    <div><p className="text-blue-500">Hello Monolingo</p></div>
+    <div><p className="text-blue-500">Hello Monolingo</p>
+    <Button>Click Here</Button>
+    </div>
   )
 }
