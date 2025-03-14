482619b919ec6f95fc86ab898b18913f05938add
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-14 16:02:31 +0000

    3D buttons

--- mongolingo/app/page.tsx
+++ mongolingo/app/page.tsx
@@ -1,8 +1,8 @@
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 export default function Home() {
-  return(
+  return (
     <div><p className="text-blue-500">Hello Monolingo</p>
-    <Button>Click Here</Button>
+      <Button>Click Here</Button>
     </div>
   )
 }
