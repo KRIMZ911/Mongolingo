451ad337cb1ad4fb83b44fa5f46165193f65f2c6
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-17 00:04:23 +0000

    Buttons

--- mongolingo/app/buttons/page.tsx
+++ mongolingo/app/buttons/page.tsx
@@ -1,13 +1,23 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const ButtonsPage = () => {
-    return (
-        <div className="p-4 space-y-4 flex flex-col max-w-[200px]">Buttons Page!!!
-            <Button>Default</Button>
-            <Button variant="primary">Primary</Button>
-            <Button>Priamry Outline</Button>
-        </div>
-    )
-}
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      Buttons Page!!!
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline"> Priamry Outline</Button>
+      <Button variant="secondary">Secondary </Button>
+      <Button variant="secondaryOutline"> Secondary Outline</Button>
+      <Button variant="danger">danger </Button>
+      <Button variant="dangerOutline"> danger Outline</Button>
+      <Button variant="super">super </Button>
+      <Button variant="superOutline"> super Outline</Button>
+      <Button variant="ghost">ghost </Button>
+      <Button variant="sidebar">sidebar </Button>
+      <Button variant="sidebarOutline"> sidebar Outline</Button>
+    </div>
+  );
+};
 
 export default ButtonsPage;
--- mongolingo/components/ui/button.tsx
+++ mongolingo/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive uppercase tracking-wide font-bold",
@@ -11,15 +11,32 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-white text-blue border-slate-200 border-2 border-b-[4px] active:border-b-[2px] hover:bg-slate-100 text-slate-500",
-        primary: "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-[4px] active:border-b-0",
-        primaryOutline: "bg-white",
+        primary:
+          "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-[4px] active:border-b-0",
+        primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
+
+        secondary:
+          "bg-green-500 text-primary-foreground hover:bg-green-400/90 border-green-600 border-b-[4px] active:border-b-0",
+        secondaryOutline: "bg-green text-green-500 hover:bg-slate-100",
+        danger:
+          "bg-rose-500 text-primary-foreground hover:bg-rose-400/90 border-rose-600 border-b-[4px] active:border-b-0",
+        dangerOutline: "bg-rose text-rose-500 hover:bg-slate-100",
+        super:
+          "bg-indigo-500 text-primary-foreground hover:bg-indigo-400/90 border-indigo-600 border-b-[4px] active:border-b-0",
+        superOutline: "bg-indigo text-indigo-500 hover:bg-slate-100",
+        ghost:
+          "bg-trasnparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
+        sidebar:
+          "bg-transparent text-slate-500 border-2 border-transparent hover:bg-slate-100 transition-none",
+        sidebarOutline:
+          "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
       },
       size: {
         default: "h-15 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8  gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10  px-6 has-[>svg]:px-4",
         icon: "size-9",
-        rounded: "rounded-full"
+        rounded: "rounded-full",
       },
     },
     defaultVariants: {
@@ -27,7 +44,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 function Button({
   className,
@@ -37,9 +54,9 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
@@ -47,7 +64,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
