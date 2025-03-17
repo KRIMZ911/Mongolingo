1856f71ebb1e150ebbf9f29c0d6ef0677cbd3d6b
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-17 23:06:32 +0000

    Layout along with footer and header Plus added clerk

---
+++ mongolingo/app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div>
+      <p className="text-blue-500">Hello Dota Monolingo</p>
+      <Button>Click Here</Button>
+    </div>
+  );
+}
--- mongolingo/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button"
-export default function Home() {
-  return (
-    <div><p className="text-blue-500">Hello Monolingo</p>
-      <Button>Click Here</Button>
-    </div>
-  )
-}
