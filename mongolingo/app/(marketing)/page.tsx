b8ff9bf9b48ea47143a1d92e35c36ecaae5fbcdb
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-18 16:59:48 +0000

    Buttons

--- mongolingo/app/(marketing)/page.tsx
+++ mongolingo/app/(marketing)/page.tsx
@@ -1,9 +1,35 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import { ClerkLoading, ClerkLoaded, SignedOut, SignedIn } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-blue-500">Hello Dota Monolingo</p>
-      <Button>Click Here</Button>
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h[424px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Lear, pracice and master class with Mongolingo
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            {" "}
+            <SignedOut>
+              <SighnUpButton>
+                <Button size="lg" variant="secindary" className="w-full">
+                  Get Stared
+                </Button>
+              </SighnUpButton>
+            </SignedOut>
+            <SignedIn></SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
