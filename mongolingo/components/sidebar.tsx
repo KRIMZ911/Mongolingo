c5739510a9c79935b2d26a59332fa3d5885ab723
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-22 01:28:21 +0000

    sidebar

--- mongolingo/components/sidebar.tsx
+++ mongolingo/components/sidebar.tsx
@@ -1,5 +1,15 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebar-item";
+import {
+  ClerkLoading,
+  ClerkLoaded,
+  UserButton,
+  ClerkProvider,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
 type Props = { className?: string };
 
 export const Sidebar = ({ className }: Props) => {
@@ -9,11 +19,33 @@ export const Sidebar = ({ className }: Props) => {
         "bg-blue-500 h-full fixed w-fill lg:w-[256px] top-0 left-0 px-4 border-r-2 flex-col"
       )}
     >
-      <div className="pt-8 pl-7 flex items-center gap-x-3">
-        <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
-        <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
-          Mongolingo
-        </h1>
+      <Link href="/learn">
+        <div className="pt-8 pl-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Mongolingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="/learn.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem label="quests" href="/quests" iconSrc="/quests.svg" />
+        <SidebarItem label="shop" href="/shop" iconSrc="/shop.svg" />
+      </div>
+      <div className="p-4 fixed bottom-0">
+        <ClerkProvider>
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+        </ClerkProvider>
       </div>
     </div>
   );
