159ea4ddb7ef9187c2db0b446611af6ceabe4f4a
Bers Batjargal <115825903+KRIMZ911@users.noreply.github.com>
2025-03-23 00:11:39 +0000

    make sticky wrapper

--- mongolingo/app/(main)/learn/page.tsx
+++ mongolingo/app/(main)/learn/page.tsx
@@ -1,9 +1,14 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+
 const LearnPage = () => {
   return (
     <div>
-      <h1>Learn page</h1>
+      <div className="flex flex-row-reverse gap-[48px] px-6">
+        <StickyWrapper>Learn page sticky</StickyWrapper>
+        <FeedWrapper>My feedwrapper</FeedWrapper>
+      </div>
     </div>
   );
-}
+};
 
 export default LearnPage;
